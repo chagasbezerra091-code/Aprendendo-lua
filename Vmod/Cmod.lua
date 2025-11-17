f481f76c88ff5b3a5067273dfd377d021500dd5e
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-11-16 21:24:08 -0300

    Update and rename Cmod.lua to Vmod/Cmod.lua

---
+++ Vmod/Cmod.lua
@@ -0,0 +1,13 @@
+-- Skins
+man = "models/persons/man.3d_obeject/"
+woman = "models/persons/woman.3d_obeject/"
+RGB = true
+sRGB = false
+--menu
+enter_to_game = "1",
+enter_to_singleplayer_mode = "2",
+enter_to_normal_mode = "3",
+enter_to_script_executor_mode = "4"
+print("Enter in mode...")
+--info
+package "com.cmod.main"
